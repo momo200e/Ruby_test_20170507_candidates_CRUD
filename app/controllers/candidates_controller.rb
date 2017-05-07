81c2296fde2d4fecd7f8b1347ca09a686cae09b0
ss910316 <ss910316@gmail.com>
2017-05-07 10:15:32 -0400

    finish edit candidate feature

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -6,13 +6,26 @@ class CandidatesController < ApplicationController
       @candidate = Candidate.new
     end
  def create
-      @candidate=Candidate.new(candidate_params)
+      @candidate = Candidate.new(candidate_params)
    if@candidate.save
      redirect_to candidates_path, notice: "候選人新增成功!!"
    else
      render :new
    end
  end
+  
+  def edit
+    @candidate=Candidate.find_by(id: params[:id])
+  end
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path,notice: "候選人編輯成功!!"
+    else
+      render :edit
+    end
+  end
+  
   private
   def candidate_params
     params.require(:candidate).permit(:name,:party,:age,:politics)
