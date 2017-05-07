223234cf6d50ea05e89fe0e63447100a15fe5b2b
ss910316 <ss910316@gmail.com>
2017-05-07 09:26:54 -0400

    finish add candidate feature

---
+++ db/migrate/20170506161626_create_candidates.rb
@@ -0,0 +1,12 @@
+class CreateCandidates < ActiveRecord::Migration[5.0]
+  def change
+    create_table :candidates do |t|
+      t.string :name
+      t.string :party
+      t.integer :age
+      t.text :politics
+      t.integer :votes, default: 0
+      t.timestamps
+    end
+  end
+end
