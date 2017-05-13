015afe503656c39de563a159b45f5d8ebdaa4a97
ss910316 <ss910316@gmail.com>
2017-05-13 10:46:46 -0400

    add layout 'backend'

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,4 +1,6 @@
 class CandidatesController < ApplicationController
+  layout "backend"
+  before_action :find_candidate_id, only: [:edit,:update,:destroy,:vote]
   def index
    @candidates = Candidate.all
   end
@@ -6,7 +8,6 @@ class CandidatesController < ApplicationController
       @candidate = Candidate.new
     end
   def show
-    @candidate = Candidate.find_by(id: params[:id])
   end
  def create
       @candidate = Candidate.new(candidate_params)
@@ -18,24 +19,24 @@ class CandidatesController < ApplicationController
  end
   
   def edit
-    @candidate=Candidate.find_by(id: params[:id])
   end
   def update
-    @candidate = Candidate.find_by(id: params[:id])
     if @candidate.update(candidate_params)
       redirect_to candidates_path,notice: "候選人編輯成功!!"
     else
       render :edit
     end
   end
+  
+  
   def destroy
-    @candidate = Candidate.find_by(id: params[:id])
+   
     @candidate.destroy if @candidate
       redirect_to candidates_path, notice: "候選人資料已刪除!"
   end
     
   def vote
-    @candidate = Candidate.find_by(id: params[:id])
+    
     @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate
     redirect_to candidates_path,notice: "投票成功!"
     
@@ -44,5 +45,8 @@ class CandidatesController < ApplicationController
   private
   def candidate_params
     params.require(:candidate).permit(:name,:party,:age,:politics)
+  end
+  def find_candidate_id
+     @candidate = Candidate.find_by(id: params[:id])
   end
 end
